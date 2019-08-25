5d77e047e963b14a751ab921a2b53d326c729b46
ning0825 <1010411052@qq.com>
2019-08-26 00:17:00 +0800

    add list

--- lib/newMain.dart
+++ lib/newMain.dart
@@ -1,3 +1,8 @@
+import 'dart:io';
+import 'dart:ui' as prefix0;
+import 'dart:developer' as developer;
+import 'package:flutter/rendering.dart';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -6,10 +11,12 @@ void main() => runApp(NewApp());
 class NewApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    debugPaintSizeEnabled = false;
     // TODO: implement build
     return MaterialApp(
       home: NewHome(),
       debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
     );
   }
 }
@@ -19,15 +26,51 @@ class NewHome extends StatefulWidget {
   State<StatefulWidget> createState() => NewHomeState();
 }
 
-class NewHomeState extends State<NewHome> {
+class NewHomeState extends State<NewHome> with TickerProviderStateMixin {
+  var currentPage = 0.0;
+
+  var cards = [oweCard(), loanCard()];
+
+  AnimationController _animationController;
+  AnimationController _animationController2;
+  Animation<double> showAnimation;
+  Animation<double> hideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 400));
+    showAnimation = Tween(begin: 0.0, end: 1.0).animate(_animationController);
+    showAnimation.addListener(() {
+      setState(() {});
+    });
+
+    _animationController2 =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 400));
+    hideAnimation = Tween(begin: 0.0, end: 1.0).animate(_animationController2);
+    hideAnimation.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var _controller = PageController(viewportFraction: 0.85);
+    _controller.addListener(() {
+      setState(() {
+        //todo set opacity animation
+        currentPage = _controller.page;
+      });
+    });
+
     // TODO: implement build
     return Scaffold(
       body: Column(
         children: <Widget>[
           Padding(
-              padding: EdgeInsets.all(10),
+              padding:
+                  EdgeInsets.only(top: 20, left: 30, right: 30, bottom: 20),
               child: SafeArea(
                 child: Row(
                   children: <Widget>[
@@ -40,8 +83,11 @@ class NewHomeState extends State<NewHome> {
                     ),
                     Expanded(
                         child: Text(
-                      'bank',
-                      style: TextStyle(color: Colors.white, fontSize: 20),
+                      '-bank',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold),
                       textAlign: TextAlign.center,
                     )),
                     IconButton(
@@ -60,52 +106,75 @@ class NewHomeState extends State<NewHome> {
               height: 50,
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(26),
-                      topRight: Radius.circular(26)),
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40)),
                   color: Colors.white),
+              margin: EdgeInsets.only(top: 20),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(left: 40, top: 30),
+                    child: Text(
+                      'Cards',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                    ),
+                  ),
                   Container(
-                    padding: EdgeInsets.all(40),
-                    margin: EdgeInsets.all(40),
-                    width: double.infinity,
-                    height: 200,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(40)),
-                        color: Colors.amber),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text('本月需还:'),
-                        Text('已       还:'),
-                        Text('未       还:')
-                      ],
+                    width: double.maxFinite,
+                    height: 300,
+                    child: PageView.builder(
+                        itemBuilder: (BuildContext context, int position) {
+                          if (position == currentPage) {
+                            return cards[position];
+                          } else {
+                            return Transform.scale(
+                              scale: 1 - (currentPage - position).abs() * 0.1,
+                              child: cards[position],
+                            );
+                          }
+                        },
+                        itemCount: 2,
+                        physics: BouncingScrollPhysics(),
+                        controller: _controller,
+                        onPageChanged: (index) {
+                          developer.log('animation exec');
+                          if (index == 0) {
+                            _animationController.forward(from: 0.0);
+                            _animationController2.reverse(from: 1.0);
+                          } else if (index == 1) {
+                            _animationController2.forward(from: 0.0);
+                            _animationController.reverse(from: 1.0);
+                          }
+                        }),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 40, top: 10, bottom: 10),
+                    child: Text(
+                      'Transactions',
+                      style:
+                          TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
                     ),
                   ),
                   Expanded(
-                    child: ListView.builder(
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          leading: IconButton(
-                            icon: Icon(Icons.ac_unit),
-                          ),
-                          title: Text('title'),
-                          subtitle: Text('subtitle'),
-                          isThreeLine: true,
-                          trailing: IconButton(
-                            icon: Icon(
-                              Icons.image,
-                              color: Colors.red,
-                            ),
-                            onPressed: () => {},
+                    child: currentPage == 0
+                        ? FadeTransition(
+                            child: ListView.builder(
+                                itemBuilder: (context, index) {
+                                  return listTileWidget(index);
+                                },
+                                itemCount: 10),
+                            opacity: showAnimation,
                           )
-
-                        );
-                      },
-                      itemCount: 50,
-                      itemExtent: 40,
-                    ),
+                        : FadeTransition(
+                            child: ListView.builder(
+                                itemBuilder: (context, index) {
+                                  return ListTile(title: Text('$index'),);
+                                },
+                                itemCount: 10),
+                            opacity: hideAnimation,
+                          ),
                   )
                 ],
               ),
@@ -114,6 +183,115 @@ class NewHomeState extends State<NewHome> {
         ],
       ),
       backgroundColor: Colors.black,
+    );
+  }
+
+  void changeListOpacity(index) {
+    _animationController.forward();
+  }
+
+  Widget listTileWidget(int index) {
+    return Padding(
+      padding: EdgeInsets.only(left: 40, top: 10, bottom: 10, right: 30),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                '分期乐',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 8),
+                child: Text('09-11'),
+              )
+            ],
+          ),
+          Spacer(),
+          IconButton(
+            icon: Icon(
+              Icons.all_inclusive,
+              size: 30,
+            ),
+            onPressed: null,
+            padding: EdgeInsets.all(2),
+          )
+        ],
+      ),
+    );
+  }
+
+  static Widget oweCard() {
+    return Padding(
+      padding: EdgeInsets.only(top: 40, bottom: 40),
+      child: Container(
+        padding: EdgeInsets.all(40),
+        width: double.infinity,
+        height: 280,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+            boxShadow: [
+              BoxShadow(
+                  spreadRadius: 0, blurRadius: 30, color: Colors.grey[400])
+            ],
+            image: DecorationImage(
+                image: AssetImage('assets/CardBG.webp'), fit: BoxFit.fill)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              '本月需还:',
+              style: TextStyle(color: Colors.white),
+            ),
+            Text(
+              '已       还:',
+              style: TextStyle(color: Colors.white),
+            ),
+            Text(
+              '未       还:',
+              style: TextStyle(color: Colors.white),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  static Widget loanCard() {
+    return Padding(
+      padding: EdgeInsets.only(top: 40, bottom: 40),
+      child: Container(
+        padding: EdgeInsets.all(40),
+        width: double.infinity,
+        height: 280,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+            boxShadow: [
+              BoxShadow(
+                  spreadRadius: 0, blurRadius: 12, color: Colors.grey[400])
+            ],
+            image: DecorationImage(
+                image: AssetImage('assets/CardBG.webp'), fit: BoxFit.fill)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              '本月需借:',
+              style: TextStyle(color: Colors.white),
+            ),
+            Text(
+              '已       借:',
+              style: TextStyle(color: Colors.white),
+            ),
+            Text(
+              '未       借:',
+              style: TextStyle(color: Colors.white),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
